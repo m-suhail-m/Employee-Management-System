61f71b225f9e8ecef055b6dee6ccbb44e2784170
m-suhail-m <101105845+m-suhail-m@users.noreply.github.com>
2022-09-18 20:32:30 +0200

    api connection string fix

--- Employee-Management-System-Frontend/src/app/Services/http.service.ts
+++ Employee-Management-System-Frontend/src/app/Services/http.service.ts
@@ -8,9 +8,9 @@ export class HttpService {
 
   constructor() { }
 
-  // httpLink:string = 'http://msuhailm-001-site1.htempurl.com/api/'
+  httpLink:string = 'http://msuhailm-001-site1.htempurl.com/api/'
 
-  httpLink:string = 'https://localhost:44357/api/'
+  // httpLink:string = 'https://localhost:44357/api/'
 
   FetchError(){
     alert("An error occurred while trying to fetch the data")
